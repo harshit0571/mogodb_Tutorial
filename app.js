d9ea9c1663c23fc6985e27c66fc0285ab5424cc7
harshit0571 <harshitchadha65@gmail.com>
2022-05-28 20:51:42 +0530

    added .env file to store link of mogo database monogodb connected throught url parser finnaly working

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-
-
-
-
-
-
-console.log("working")
+mongoose.connect(process.env.link, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("working")
+}).catch((err) => console.log(err));
