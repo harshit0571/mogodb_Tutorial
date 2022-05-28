1c7c26090f91b84b8a741e4c8e23c9ab90c46485
harshit0571 <harshitchadha65@gmail.com>
2022-05-28 21:05:33 +0530

    new user created and saved in collections

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const user = require("./user")
 require('dotenv').config()
 
 mongoose.connect(process.env.link, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("working")
 }).catch((err) => console.log(err));
+
+
+const new_user = new user({ username: "harshit", password: "5545454511" })
+new_user.save().then(() => console.log(new_user))
