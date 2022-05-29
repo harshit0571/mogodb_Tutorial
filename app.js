8214a65328f1ec6f84ef05f31e643b9b4c70b47f
harshit0571 <harshitchadha65@gmail.com>
2022-05-29 11:13:46 +0530

    find by id method

--- app.js
+++ app.js
@@ -7,5 +7,17 @@ mongoose.connect(process.env.link, { useNewUrlParser: true, useUnifiedTopology: 
 }).catch((err) => console.log(err));
 
 
-const new_user = new user({ username: "harsh", password: "5545454511" })
-new_user.save().then(() => console.log(new_user))
+// const new_user = new user({ username: "harsh", password: "5545454511" })
+// new_user.save().then(() => console.log(new_user))
+
+async function run() {
+    try {
+        const find = await user.findById("629240f3a29260a4b5766d4d");
+        console.log(find);
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+}
+run();
